2eb1e685effd15a81832698238d654554cf66033
Vinicio Chavarria <vichavarria21@gmaail.com>
2023-10-04 20:25:12 -0600

    Footer component css responsivo

--- src/app/Shared/shared.module.ts
+++ src/app/Shared/shared.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { EmailSignUpComponent } from './components/email-sign-up/email-sign-up.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
     NavBarComponent,
-    EmailSignUpComponent
+    EmailSignUpComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     NavBarComponent,
-    EmailSignUpComponent
+    EmailSignUpComponent,
+    FooterComponent
   ]
 })
 export class SharedModule { }
