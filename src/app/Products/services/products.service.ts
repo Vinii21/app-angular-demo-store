c777604847bda42c8918621ad2a830bd88300968
Vinicio Chavarria <vichavarria21@gmaail.com>
2023-10-06 22:01:16 -0600

    Actualización del filtro

--- src/app/Products/services/products.service.ts
+++ src/app/Products/services/products.service.ts
@@ -15,8 +15,20 @@ export class ProductsService {
       price: 100
     },
     {
-      url: "https://images.unsplash.com/photo-1581539250439-c96689b516dd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1365&q=80",
-      name: "silla plastica alta",
+      url: "https://images.unsplash.com/photo-1580480055273-228ff5388ef8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80",
+      name: "silla patas madera",
+      category: "chair",
+      price: 260
+    },
+    {
+      url: "https://images.unsplash.com/photo-1503602642458-232111445657?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80",
+      name: "Banco blanco",
+      category: "chair",
+      price: 50
+    },
+    {
+      url: "https://images.unsplash.com/photo-1540809799-5da9372c3f64?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1448&q=80",
+      name: "Banco mini par",
       category: "chair",
       price: 90
     },
@@ -27,10 +39,34 @@ export class ProductsService {
       price: 300
     },
     {
+      url: "https://images.unsplash.com/photo-1540574163026-643ea20ade25?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+      name: "sofa tela opaca",
+      category: "sofa",
+      price: 370
+    },
+    {
+      url: "https://images.unsplash.com/photo-1573866926487-a1865558a9cf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1471&q=80",
+      name: "sofa delux",
+      category: "sofa",
+      price: 500
+    },
+    {
       url: "https://images.unsplash.com/photo-1572025442348-511bdcae389b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80",
       name: "comedor 4 puestos",
       category: "comedor",
       price: 630
+    },
+    {
+      url: "https://images.unsplash.com/photo-1522683280249-025c6e03d311?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80",
+      name: "comedor 11 puestos",
+      category: "comedor",
+      price: 950
+    },
+    {
+      url: "https://plus.unsplash.com/premium_photo-1684505553482-9de8b2d437cc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDN8fGNvbWVkb3J8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60",
+      name: "comedor redondo 4 puestos",
+      category: "comedor",
+      price: 450
     },
   ]
   private cardsSubject = new BehaviorSubject<Product[]>(this.cards);
@@ -41,7 +77,9 @@ export class ProductsService {
     const numOne = Number(prices[0]);
     const numTwo = Number(prices[1] || "20000000000000");
     let filteredCards = this.cards;
-    if(!todo) {
+    if(todo) {
+      filteredCards = this.cards.filter(c => c.price! >= numOne && c.price! <= numTwo);
+    } else {
       if (silla) filteredCards = this.cards.filter(c => (c.category === 'chair')&&(c.price! >= numOne && c.price! <= numTwo))
       if (sofa) filteredCards = this.cards.filter(c => (c.category === 'sofa')&&(c.price! >= numOne && c.price! <= numTwo))
       if (comedor) filteredCards = this.cards.filter(c => (c.category === 'comedor')&&(c.price! >= numOne && c.price! <= numTwo))
