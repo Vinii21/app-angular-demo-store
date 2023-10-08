0e2394741197a1c3269b1734b930ec81758ceab5
Vinicio Chavarria <vichavarria21@gmaail.com>
2023-10-08 16:18:42 -0600

    Componentes actualizados, modulo de contacto, css y diseño responsivo

---
+++ src/app/Products/components/one-product-view/one-product-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'oneProduct-view',
+  templateUrl: './one-product-view.component.html',
+  styleUrls: ['./one-product-view.component.css']
+})
+export class OneProductViewComponent {
+
+}
