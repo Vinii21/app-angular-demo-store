521a287e2023bacb8b21df141238cf0b8cf05db2
Vinicio Chavarria <vichavarria21@gmaail.com>
2023-10-09 18:41:58 -0600

    Actualización para acceder a vista donde solo se ve un producto

--- src/app/Products/components/one-product-view/one-product-view.component.ts
+++ src/app/Products/components/one-product-view/one-product-view.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'oneProduct-view',
   templateUrl: './one-product-view.component.html',
   styleUrls: ['./one-product-view.component.css']
 })
-export class OneProductViewComponent {
+export class OneProductViewComponent implements OnInit {
+  constructor (private activatedRoute: ActivatedRoute) {}
 
+  ngOnInit(): void {
+    this.activatedRoute.params
+      .subscribe((params) => {
+        console.log({params: params["id"]})
+      })
+  }
 }
--- src/app/Products/components/product-list/product-list.component.ts
+++ src/app/Products/components/product-list/product-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/Home/interfaces/products.interface';
 import { ProductsService } from '../../services/products.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'product-list',
